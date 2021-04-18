1bc6fc01536386430ad8caf19361f09b598f02fa
Monimoy Bujarbaruah <monimoy_07@yahoo.co.in>
2021-04-17 18:38:42 -0700

    Add files via upload
    required function for bounds

---
+++ eval_binBound.m
@@ -0,0 +1,15 @@
+%% evaluate that binomial sum for new bounds
+
+function [val] = eval_binBound(AnomD, N_thres, N, delmatD, Fxrow, boldAvbar, nx)
+
+val = 0; 
+
+for j = 1:N-1
+    for i = max(N_thres, j):N-1
+        val = val + nchoosek(i,j)*norm(AnomD,inf)^(i-j)*norm(delmatD, inf)^j*...
+                                            norm(Fxrow*boldAvbar(:,(i-1)*nx*N+1: i*nx*N),1);
+    end
+end
+
+
+end
