269a6356ed975a44bcbfa73f2b18d160e5e569f1
Monimoy Bujarbaruah <monimoy_07@yahoo.co.in>
2021-04-19 04:51:07 -0700

    Update sys_load.m
    vertflag removed

--- sys_load.m
+++ sys_load.m
@@ -1,65 +1,45 @@
 %% Function defining System and Constraint Matrices 
 %  Monimoy Bujarbaruah
 
-function [Anom,Bnom, delAv, delBv, K, A, B, X, U, Xlb, Xub, Ulb, Uub, nx, nu, wub,wlb, Q, R, N_max, N_thres] = sys_load(vertflag, epsA, epsB)
+function [Anom,Bnom, delAv, delBv, K, A, B, X, U, Xlb, Xub, Ulb, Uub, nx, nu, wub,wlb, Q, R, N_max, N_thres] = sys_load(epsA, epsB)
 
-%% Considering two states and one scalar input 
+    %% Considering two states and one scalar input 
 
-Anom = [1, 0.15; 0.1, 1];
-Bnom = [0.1; 1.1];                    
-nx = size(Anom,2); nu = size(Bnom,2); 
-
-%%% Now read the vertflag. Value 1 means full set of vertices necessary, given a norm
-%%% representation of uncertainty. Value 0 means structured uncertainty representing vertices.  
-
-if vertflag == 1
-    %%%%%%%%% Get the error vertices matrices
-     delAv = [ [epsA, 0; epsA, 0], [epsA, 0; 0 epsA], [epsA, 0; -epsA, 0], [epsA, 0; 0, -epsA],...
-                 [0, epsA; epsA, 0], [0, epsA; 0, epsA], [0, epsA; -epsA, 0], [0, epsA; 0, -epsA],...
-                 [-epsA, 0; epsA, 0], [-epsA, 0; 0 epsA], [-epsA, 0; -epsA, 0], [-epsA, 0; 0, -epsA],...
-                 [0, -epsA; epsA, 0], [0, -epsA; 0, epsA], [0, -epsA; -epsA, 0], [0, -epsA; 0, -epsA]];    
-                    
+    Anom = [1, 0.15; 0.1, 1];
+    Bnom = [0.1; 1.1];                    
+    nx = size(Anom,2); nu = size(Bnom,2); 
+    
+    delAv = [ [0, epsA; epsA, 0], [0, epsA; -epsA, 0], [0, -epsA; epsA, 0], [0, -epsA; -epsA, 0]];     
     delBv = [[0; -epsB], [0; epsB], [epsB; 0], [-epsB; 0] ];
-     
-     %%%%%%%%% choose the feedback gain K here%%%
-     K = place(Anom, Bnom, [0.72; 0.65]);           
-     
-else
-     delAv = [ [0, epsA; epsA, 0], [0, epsA; -epsA, 0], [0, -epsA; epsA, 0], [0, -epsA; -epsA, 0]];     
-            
-     delBv = [[0; -epsB], [0; epsB], [epsB; 0], [-epsB; 0] ];
-       
-     %%%%%%%%% choose the feedback gain K here%%%
-     K = place(Anom, Bnom, [0.745; 0.75]);             
-end
-                                                                           
 
-%% Set the true A and B matrices (satisfy the above bounds)
-A = [1, 0.1; 
-       0, 1.0 ]; 
+    %%%%%%%%% choose the feedback gain K here%%%
+    K = place(Anom, Bnom, [0.745; 0.75]);             
+    
+    %% Set the true A and B matrices (satisfy the above bounds)
+    A = [1, 0.1; 
+           0, 1.0 ]; 
+    B = [0; 
+         1.0];                                                                                                                       
 
-B = [0; 
-     1.0];                                                                                                                       
+    %% Weights
+    Q =  10*eye(nx);
+    R =   2*eye(nu);
 
-%% Weights
-Q =  10*eye(nx);
-R =   2*eye(nu);
+    %% Horizon and cut-off for bounds
+    N_max = 3;             % max horizon N in the paper. Go up to 4 for good results 
+    N_thres = 3;           % beyond this value do the binomial tail. 
 
-%% Horizon and cut-off for bounds
-N_max = 3;             % max horizon N in the paper 
-N_thres = 3;           % beyond this value do the binomial tail. 
- 
-%% Considering constraints of the form -a<=x(i)<=a and -ulb<=u<=uub
-Xlb = -[8; 8];
-Xub = -Xlb; 
-Ulb =  -4; 
-Uub = -Ulb; 
+    %% Considering constraints of the form -a<=x(i)<=a and -ulb<=u<=uub
+    Xlb = -[8; 8];
+    Xub = -Xlb; 
+    Ulb =  -4; 
+    Uub = -Ulb; 
 
-X = Polyhedron('lb',Xlb,'ub',Xub);
-U = Polyhedron('lb',Ulb,'ub',Uub);                                            
-%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Disturbance  Bounds 
-wub = 0.1;                                    % Upper bound of additive noise value
-wlb = -0.1;                                   % Lower bound of additive noise value
+    X = Polyhedron('lb',Xlb,'ub',Xub);
+    U = Polyhedron('lb',Ulb,'ub',Uub);                                            
+    %%%%%%%%%%%%%%%%%%%%%%%%%
+    %% Disturbance  Bounds 
+    wub = 0.1;                                    % Upper bound of additive noise value
+    wlb = -0.1;                                   % Lower bound of additive noise value
 
 end
