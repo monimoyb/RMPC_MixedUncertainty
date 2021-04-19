487c92e115cce9b9a4bfed996a7d0009e8df2264
Monimoy Bujarbaruah <monimoy_07@yahoo.co.in>
2021-04-19 05:08:12 -0700

    Update eval_binBound.m
    indents fixed

--- eval_binBound.m
+++ eval_binBound.m
@@ -1,15 +1,14 @@
-%% evaluate that binomial sum for new bounds
+%% Evaluate a required binomial sum for bounds
+% Monimoy Bujarbaruah
 
 function [val] = eval_binBound(AnomD, N_thres, N, delmatD, Fxrow, boldAvbar, nx)
 
-val = 0; 
-
-for j = 1:N-1
-    for i = max(N_thres, j):N-1
-        val = val + nchoosek(i,j)*norm(AnomD,inf)^(i-j)*norm(delmatD, inf)^j*...
-                                            norm(Fxrow*boldAvbar(:,(i-1)*nx*N+1: i*nx*N),1);
+    val = 0; 
+    for j = 1:N-1
+        for i = max(N_thres, j):N-1
+            val = val + nchoosek(i,j)*norm(AnomD,inf)^(i-j)*norm(delmatD, inf)^j*...
+                                    norm(Fxrow*boldAvbar(:,(i-1)*nx*N+1: i*nx*N),1);
+        end
     end
-end
-
 
 end
